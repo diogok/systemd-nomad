0ff3eb17ef95b2a8ac4550691b44856044107bae
Diogo Silva <diogo@diogok.net>
2019-04-14 09:50:20 -0300

    Update consul and nomad version

--- scripts/30-consul.sh
+++ scripts/30-consul.sh
@@ -7,7 +7,7 @@ command -v consul && exit 0 || true
 
 sudo mkdir -p /var/lib/consul
 
-version=1.4.2
+version=1.4.4
 arch=$(arch)
 [[ "$arch" == "armv6l" ]] && arch="arm"
 [[ "$arch" == "x86_64" ]] && arch="amd64"
